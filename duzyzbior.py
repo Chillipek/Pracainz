e3fb25d682740202508b1429c8a9fafcf801eeb5
Chillipek <czop.filip@gmail.com>
2023-06-17 19:09:31 +0200

    Added script and picture

---
+++ duzyzbior.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+# Załadowanie modelu VGG Net
+model = VGG16(weights='imagenet')
+
+# Wczytanie obrazu z pliku
+img_path = 'gory_1.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+# Dokonanie klasyfikacji obrazu
+preds = model.predict(x)
+print('Predicted:', decode_predictions(preds, top=3)[0])
